90e7d02878ded213fc71d2a89854aa8aa5727330
jkurambhatti <jkurambhatti2992@gmail.com>
2016-09-14 21:23:55 +0530

    added scraper for links and images, can also download all images on given page

--- newscraper.go
+++ newscraper.go
@@ -5,38 +5,84 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
-	// "io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
+var link string
+var name int
+
 func main() {
-	// seedurls := os.Args[1:]
-	v := os.Args[1]
-	fmt.Println(v)
-	res, err := http.Get(v)
-	if res.StatusCode != http.StatusOK {
-		fmt.Println(err)
-	}
-
-	nodes, _ := html.Parse(res.Body)
-
-	for _, link := range do(nil, nodes) {
-		fmt.Println(link)
+	link = os.Args[1]
+	fmt.Println(link)
+	parser(link)
+	for {
 	}
 }
 
-func do(links []string, n *html.Node) []string {
+func parser(link string) {
+
+	res, err := http.Get(link)
+	if res.StatusCode != http.StatusOK {
+		fmt.Println(err)
+	}
+	defer res.Body.Close()
+
+	nodes, _ := html.Parse(res.Body)
+	// getlinks(nodes)
+	getimageslink(nodes)
+}
+
+func getlinks(n *html.Node) {
 	if n.Type == html.ElementNode && n.Data == "a" {
 		for _, at := range n.Attr {
 			if at.Key == "href" {
-				links = append(links, at.Val)
+				if strings.HasSuffix(at.Val, ".php") {
+					fmt.Println(at.Val)
+					// go getimageslink(os.Args[1] + at.Val)
+				}
 			}
 		}
 	}
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		links = do(links, c)
+		getlinks(c)
 	}
-	return links
+
+}
+
+func getimageslink(n *html.Node) {
+	if n.Type == html.ElementNode && n.Data == "img" {
+		for _, at := range n.Attr {
+			if at.Key == "src" {
+				go downloadimages(at.Val)
+			}
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		getimageslink(c)
+	}
+}
+
+func downloadimages(imglink string) {
+	fmt.Println(imglink)
+	r, err := http.Get(imglink)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	name++
+	f, err := os.Create(fmt.Sprint("image", name, ".jpg"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	io.Copy(f, r.Body)
+	f.Close()
+	r.Body.Close()
 }
